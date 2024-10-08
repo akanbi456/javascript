2d19e4ca1a527851efbd43a96913f5aeb037c45b
akanbi muhammed <muhammedakanbi041@gmail.com>
2024-10-08 12:02:36 +0100

    add all

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+
+let username = prompt("what's your name?")
+let age =prompt('how old are you?')
+let gender = prompt('what your sex')
+let sch =prompt('which school do you attend')
+
+
+document.getElementById('heading').innerHTML=('your name is '+ username +'i am ' + age + 'i am a '+ gender +'I attended ' + sch)
+
+
+
+
+let input =document.querySelector('input')
+
+console.log(input.value)
